603bd4efa05b77bcfe893d9f59d8688199f36731
Jordan Winslow <admin@jordanwinslow.me>
2019-08-10 21:57:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,71 +53,36 @@ function checkCollision(rock) {
   }
 }
 
-/*
 function createRock(x) {
   const rock = document.createElement('div')
 
   rock.className = 'rock'
   rock.style.left = `${x}px`
 
-  // Hmmm, why would we have used `var` here?
-  var top = 0
+  var top = rock.style.top = 0
 
-  rock.style.top = top
+  GAME.appendChild(rock)
 
-  /**
-   * Now that we have a rock, we'll need to append
-   * it to GAME and move it downwards.
-   */
-   
-   GAME.appendChild(rock)
-   window.requestAnimationFrame(moveRock(rock))
-}
+  function moveRock() {
+    rock.style.top = `${top += 2}px`;
 
-  /**
-   * This function moves the rock. (2 pixels at a time
-   * seems like a good pace.)
-   */
-   
-  function moveRock(rock) {
-    
-    /**
-     * If a rock collides with the DODGER,
-     * we should call endGame()
-     */
-     
-    if(checkCollision(rock)){
-      endGame()
-      }
-    
-    /**
-     * Otherwise, if the rock hasn't reached the bottom of
-     * the GAME, we want to move it again.
-     */
-     
-     if(rock.style.top < GAME_HEIGHT){
-       
-         rock.style.top = positionToInteger(rock.style.top + 2) + "px"
-     }
-    /**
-     * But if the rock *has* reached the bottom of the GAME,
-     * we should remove the rock from the DOM
-     */
-     if(rock.style.top >= GAME_HEIGHT){
-       rock.remove()
+    if (checkCollision(rock)) {
+      return endGame()
+    }
+
+    if (top < GAME_HEIGHT) {
+      window.requestAnimationFrame(moveRock)
+    } else {
+      rock.remove()
+    }
   }
 
-  // We should kick of the animation of the rock around here
- 
-  // Add the rock to ROCKS so that we can remove all rocks
-  // when there's a collision
+  window.requestAnimationFrame(moveRock)
+
   ROCKS.push(rock)
 
-  // Finally, return the rock element you've created
   return rock
 }
-
-*/
 
 /**
  * End the game by clearing `gameInterval`,
